3fef80211bdc85990760021cabe7e03e58f1aa48
mbrodt <madsbrodt3@gmail.com>
2020-03-15 22:06:13 +0100

    add recommendations

---
+++ src/components/Recommendations.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="mt-16 max-w-2xl text-lg">
+    <h2 class="text-2xl font-semibold text-gray-800">Anbefalinger</h2>
+    <p>
+      Du bør altid følge myndighedernes anbefalinger, som du kan læse på
+      <a class="text-blue-600 underline" href="https://coronasmitte.dk"
+        >coronasmitte.dk</a
+      >. Det vigtigste, du som almindelig borger kan gøre, er at forebygge
+      smitten så godt som muligt.
+    </p>
+    <ul class="list-disc list-inside mt-4">
+      <li>Hvis du er rask, så beskyt dig selv for at beskytte andre</li>
+      <li>
+        Hvis du har hoste, let feber eller forkølelse, så bliv hjemme, indtil du
+        er rask
+      </li>
+      <li>
+        Hvis du har problemer med vejrtrækningen, forværring af symptomer eller
+        er syg over flere dage, skal du ringe til lægen/vagtlægen. Det er
+        vigtigt, at du ikke bare møder op.
+      </li>
+    </ul>
+
+    <p class="mt-4">
+      Selvom du virker rask og ikke har symptomer, kan du stadig godt være
+      smittet. Derfor er det vigtigt, at du altd beskytter både dig selv og
+      andre.
+      <img
+        class="mt-4"
+        src="https://www.sst.dk/-/media/PLAKAT-NY/NY-NY-PLAKAT.ashx?h=911&la=da&w=641&hash=8004E43BEE895C8F32AC7939EBAC442D2263B53B"
+        alt=""
+      />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
